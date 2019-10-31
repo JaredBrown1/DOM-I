04d0e42d34aeff98fa17135fc84bee5ba49f1586
Jared Brown <jared@Jareds-MBP.lan1>
2019-10-30 20:33:43 -0700

    finished everything but nav bar

--- js/index.js
+++ js/index.js
@@ -42,13 +42,47 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 // let Links = document.getElementsByClassName("nav");
-// Links.setAttribute("href", siteContent["nav"]["nav-item-1"] );
+// Array.from(Links).forEach(link => {
+//   link.textContent = nav.nav-item-1;
+// });
+
+let firstText = document.querySelector(".cta-text h1");
+firstText.textContent = siteContent.cta.h1;
+
+let secText = document.querySelector(".cta-text button");
+secText.textContent = siteContent.cta.button;
 
 let secImg = document.getElementById("cta-img");
 secImg.setAttribute("src", siteContent["cta"]["img-src"]);
 
+let mainContent = siteContent["main-content"];
+
+let firstHeader = document.querySelectorAll("h4");
+firstHeader[0].textContent = mainContent["features-h4"];
+firstHeader[1].textContent = mainContent["about-h4"];
+firstHeader[2].textContent = mainContent["services-h4"];
+firstHeader[3].textContent = mainContent["product-h4"];
+firstHeader[4].textContent = mainContent["vision-h4"];
+firstHeader[5].textContent = siteContent.contact["contact-h4"];
+
+let paragraph = document.querySelectorAll("p");
+paragraph[0].textContent = mainContent["features-content"];
+paragraph[1].textContent = mainContent["about-content"];
+paragraph[2].textContent = mainContent["services-content"];
+paragraph[3].textContent = mainContent["product-content"];
+paragraph[4].textContent = mainContent["vision-content"];
+paragraph[5].textContent = mainContent["contact-content"];
+
 let threeImg = document.getElementById("middle-img");
 threeImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let contactContent = siteContent["contact"];
+
+let contact = document.querySelectorAll(".contact p");
+contact[0].textContent = contactContent.address;
+contact[1].textContent = contactContent.phone;
+contact[2].textContent = contactContent.email;
+
 
 
 
